07a59a3f568da948545341abebc7d55d8f0fe63b
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-07-25 13:23:23 -0300

    feature(database): Fake database moved to app

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const HOST = "http://my-json-server.typicode.com/costvin15/fakedatabase";
+const HOST = "http://my-json-server.typicode.com/costvin15/react-native-social-network";
 
 const request = async ({route, param}) => {
   const response = await fetch(`${HOST}/${route}`);
