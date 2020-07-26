0578fe9a1be7234b0540beb2d3705eab3bfd2ba5
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-07-26 20:11:48 -0300

    feature(pages/friends): Friends page improved
    UserItem component implemented

---
+++ src/screens/HomeSubContext/screens/Friends/components/UserItem/styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+  paddingVerticalDefault: {
+    paddingVertical: 15,
+  },
+  paddingHorizontalDefault: {
+    paddingHorizontal: 15,
+  },
+  flexDirectionRow: {
+    flexDirection: "row",
+  },
+  justifyContentCenter: {
+    justifyContent: "center",
+  },
+});
+
+export default styles;
